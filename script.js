cfd18cc31a07bbe974d9edf9df86f83cd53ae6bd
Rane-rane <chiarabellejordan0510@gmail.com>
2025-07-08 23:12:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateLogin() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  if (username === "SirJabez" && password === "amaz1ng!") {
+  if (username === "jordan" && password === "0510") {
     window.location.href = "index.html";
     return false;
   } else {
